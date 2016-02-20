5be18e9236f85efe1f3f517d4c0b59fdda7d4ad2
nicloay <nicloay@gmail.com>
2016-02-20 12:05:41 +0300

    player shoot booklets.

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class BulletController : MonoBehaviour {
+    public float Speed = 0.1f;
+
+    Rigidbody rb;
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rb.position += transform.TransformDirection( Vector3.forward) * Speed * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/FireController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class FireController : MonoBehaviour {
+    public GameObject BulletPrefab;
+    public float FirePointDistance;
+    Vector3 PreviousPosition;
+    bool directionSet = false;
+	
+    Vector3 lastKnownDirection = Vector3.zero;
+
+
+    void Update () {        
+        Vector3 currentPosition = transform.position;
+
+        if (directionSet){
+            if (currentPosition != PreviousPosition){
+                lastKnownDirection = (currentPosition - PreviousPosition).normalized;
+            }
+        }
+        #if DEBUG_RAYS
+
+        Debug.DrawRay(transform.position, lastKnownDirection, Color.black);
+        #endif
+        if (lastKnownDirection != Vector3.zero && Input.GetButton("Fire")){ //TODO: check is bullet active
+            GameObject bullet = GameObject.Instantiate(BulletPrefab);
+            Vector3 bulletPosition = transform.position + (lastKnownDirection * FirePointDistance);
+            bullet.transform.position = bulletPosition;
+            bullet.transform.LookAt(bulletPosition + lastKnownDirection);
+        }
+        PreviousPosition = currentPosition;
+        directionSet = true;
+	}        
+}
