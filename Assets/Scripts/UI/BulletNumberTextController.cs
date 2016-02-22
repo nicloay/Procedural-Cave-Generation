7b055e43061105c259d77e4c46482fae83ff7e09
nicloay <nicloay@gmail.com>
2016-02-22 12:40:15 +0300

    change bullet number while one bullet on screen.

--- Assets/Scripts/UI/BulletNumberTextController.cs
+++ Assets/Scripts/UI/BulletNumberTextController.cs
@@ -6,13 +6,19 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Text))]
 public class BulletNumberTextController : MonoBehaviour {
     Text text;
+    Color originalColor;
+    Color secondColor;
 	// Use this for initialization
 	void Start () {
         text = GetComponent<Text>();
+        secondColor = originalColor = text.color;
+        secondColor.a = 0.2f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        text.color = GameData.ActiveBulletsNumber > 0 ? secondColor : originalColor; //indicate that we can't shoot
         text.text = string.Format("Bulltest: {0}", GameData.BulletNumbber);
 	}
 }
